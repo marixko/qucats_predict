b80fedc65eab366e7105545a73cd648d0dcb8648
Lilianne Nakazono <marixko@gmail.com>
2023-02-03 13:07:58 -0800

    included galex and wise

--- code/auxiliary/correct_extinction.py
+++ code/auxiliary/correct_extinction.py
@@ -3,12 +3,11 @@ import sfdmap
 import extinction
 from paths import data_path
 import numpy as np
-
+from columns import splus, wise, galex
 def correction(data):
     chunk = data.copy(deep=True)
 
-    Base_Filters   = ['u', 'J0378', 'J0395', 'J0410', 'J0430', 'g', 'J0515', 'r', 'J0660', 'i', 'J0861', 'z']
-    Features_SPLUS = [filt+'_'+'PStotal' for filt in Base_Filters]
+    feat = galex+splus+wise
     m = sfdmap.SFDMap('/Volumes/GoogleDrive/My Drive/Research/Projects/qucats_predict/data/dustmaps')
     EBV = m.ebv(chunk.RA, chunk.DEC)
 
@@ -17,17 +16,19 @@ def correction(data):
 
     # Calculando a extinção em outros comprimentos de onda
     # Utilizando a lei de extinção de Cardelli, Clayton & Mathis.
-    Lambdas = np.array([3536., 3770., 3940., 4094., 4292., 4751., 5133., 6258., 6614., 7690., 8611., 8831.])
+    Lambdas = np.array([1549.02, 2304.74,                                                        # FUV, NUV
+                        3536, 3770, 3940, 4094, 4292, 4751, 5133, 6258, 6614, 7690, 8611, 8831,  
+                        33526.00, 46028.00])
 
     Extinctions = []
     for i in range(len(AV)):
         Extinctions.append(extinction.ccm89(Lambdas, AV[i], 3.1))
 
-    Extinction_DF = pd.DataFrame(Extinctions, columns=Features_SPLUS)
+    Extinction_DF = pd.DataFrame(Extinctions, columns=feat)
     chunk = chunk.reset_index(drop=True)
 
-    mask_99 = chunk[Features_SPLUS]==99
-    chunk[Features_SPLUS] = chunk[Features_SPLUS].sub(Extinction_DF)
-    chunk[Features_SPLUS] = chunk[Features_SPLUS].mask(mask_99, other = 99)
+    mask_99 = chunk[feat]==99
+    chunk[feat] = chunk[feat].sub(Extinction_DF)
+    chunk[feat] = chunk[feat].mask(mask_99, other = 99)
     return chunk
 
