c42593930528b10e191ee6a0de94d63da55ea29a
Lilianne <marixko@gmail.com>
2023-02-16 22:10:37 -0800

    added logs_path

--- code/auxiliary/paths.py
+++ code/auxiliary/paths.py
@@ -1,22 +1,34 @@
 import os
 
-
 mycwd = os.path.abspath(os.getcwd())
-os.chdir('..')
-back_cwd = os.path.abspath(os.getcwd())
-os.chdir(mycwd)
+if mycwd.split(os.sep)[-1] == "qucats_predict":
+    parent_cwd = mycwd
+    codes_path = os.path.join(parent_cwd,"code")
+if mycwd.split(os.sep)[-1] == "code":
+    # os.chdir('..')
+    codes_path = os.path.abspath(os.getcwd())
+    os.chdir('..')
+    parent_cwd = os.path.abspath(os.getcwd())
+    os.chdir(mycwd)
 
 # data
-data_path = os.path.join(back_cwd, 'data')
+data_path = os.path.join(parent_cwd, 'data')
 
 # raw
 raw_path = os.path.join(data_path, 'raw')
 
 # input_model
-input_path = os.path.join(data_path, 'input_model')
+input_path = os.path.join(data_path, 'input')
 
 # model
-model_path = os.path.join(back_cwd, 'trained_model')
+model_path = os.path.join(parent_cwd, 'final_model')
 
 # results
 results_path = os.path.join(data_path, 'result')
+
+# predict
+predict_path = os.path.join(codes_path, 'predict')
+
+# logs
+
+logs_path = os.path.join(parent_cwd, 'logs')
