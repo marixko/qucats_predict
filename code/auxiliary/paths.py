436da4285fc8e476ecbcdcdcd2692270141002af
Lilianne <marixko@gmail.com>
2023-02-17 17:01:18 -0800

    updated paths to work in maua server

--- code/auxiliary/paths.py
+++ code/auxiliary/paths.py
@@ -1,7 +1,8 @@
 import os
 
+maua_server=False
 mycwd = os.path.abspath(os.getcwd())
-if mycwd.split(os.sep)[-1] == "qucats_predict":
+if mycwd.split(os.sep)[-1] == "qucats_predict": #folder name in Maua server is qso_z and not qucats_predict
     parent_cwd = mycwd
     codes_path = os.path.join(parent_cwd,"code")
 if mycwd.split(os.sep)[-1] == "code":
@@ -10,28 +11,28 @@ if mycwd.split(os.sep)[-1] == "code":
     os.chdir('..')
     parent_cwd = os.path.abspath(os.getcwd())
     os.chdir(mycwd)
+    if parent_cwd.split(os.path.sep)[-1] == "qso_z":
+        maua_server=True
+
+if maua_server:
+    raw_path = "/storage/splus/Catalogues/iDR4/VAC_features"
+    results_path = "/storage/splus/Catalogues/VACs/qso_z/iDR4"
+else:
+    # raw
+    raw_path = os.path.join(data_path, 'raw')
+    # results
+    results_path = os.path.join(data_path, 'result')
 
 # data
 data_path = os.path.join(parent_cwd, 'data')
-
-# raw
-raw_path = os.path.join(data_path, 'raw')
-
 # input_model
 input_path = os.path.join(data_path, 'input')
-
 # model
 model_path = os.path.join(parent_cwd, 'final_model')
-
-# results
-results_path = os.path.join(data_path, 'result')
-
 # predict
 predict_path = os.path.join(codes_path, 'predict')
-
 # logs
 logs_path = os.path.join(parent_cwd, 'logs')
-
 # auxiliary
 aux_path = os.path.join(codes_path, 'auxiliary')
 
