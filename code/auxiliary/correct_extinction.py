301a71fcc074b689faf20bc5734cb9c2faf73fc2
Lilianne <marixko@gmail.com>
2024-01-14 20:51:41 +0000

    fix correct_extinction

--- code/auxiliary/correct_extinction.py
+++ code/auxiliary/correct_extinction.py
@@ -6,10 +6,12 @@ from auxiliary.paths import data_path
 import numpy as np
 from auxiliary.columns import splus, wise, galex
 
-def correction(data):
+def correction(data, feat=[]):
     chunk = data.copy(deep=True)
 
-    feat = galex+splus+wise
+    if not feat:
+        feat = galex + splus + wise
+
     m = sfdmap.SFDMap(os.path.join(data_path, 'dustmaps'))
     EBV = m.ebv(chunk.RA, chunk.DEC)
 
@@ -18,19 +20,30 @@ def correction(data):
 
     # Calculando a extinção em outros comprimentos de onda
     # Utilizando a lei de extinção de Cardelli, Clayton & Mathis.
-    Lambdas = np.array([1549.02, 2304.74,                                                        # FUV, NUV
-                        3536, 3770, 3940, 4094, 4292, 4751, 5133, 6258, 6614, 7690, 8611, 8831,  
-                        33526.00, 46028.00])
+    
+    lambdas_dict = {'FUVmag': 1549.02, 'NUVmag': 2304.74,
+                    'u_PStotal': 3536, 'J0378_PStotal': 3770, 'J0395_PStotal': 3940, 'J0410_PStotal': 4094,
+                    'J0430_PStotal': 4292, 'g_PStotal': 4751, 'J0515_PStotal': 5133, 'r_PStotal': 6258,
+                    'J0660_PStotal': 6614, 'i_PStotal': 7690, 'J0861_PStotal': 8611, 'z_PStotal': 8831,
+                    'W1': 33526, 'W2': 46028}
+    
+    lambdas = []
+    for mag in feat:
+        lambdas.append(lambdas_dict[mag])
+    lambdas = np.array(lambdas, dtype=float)
 
     Extinctions = []
     for i in range(len(AV)):
-        Extinctions.append(extinction.ccm89(Lambdas, AV[i], 3.1))
+        Extinctions.append(extinction.ccm89(lambdas, AV[i], 3.1))
 
     Extinction_DF = pd.DataFrame(Extinctions, columns=feat)
     chunk = chunk.reset_index(drop=True)
 
     mask_99 = chunk[feat]==99
     chunk[feat] = chunk[feat].sub(Extinction_DF)
-    chunk[feat] = chunk[feat].mask(mask_99, other = 99)
+    chunk[feat] = chunk[feat].mask(mask_99, other=99)
+    chunk.index = data.index
     return chunk
+
+
 
