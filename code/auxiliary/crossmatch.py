dd99c2030f41ed0469aa64a97780abbcf83f25c3
Lilianne <marixko@gmail.com>
2024-01-15 00:59:26 +0000

    6 failed fields now are crossmatched with unWISE

--- code/auxiliary/crossmatch.py
+++ code/auxiliary/crossmatch.py
@@ -2,33 +2,47 @@ import os
 import logging
 from auxiliary.paths import logs_path,  aux_path, save_xmatch_path
 
-logging.biasicConfig(filename=os.path.join(logs_path,'errors_field.log'), format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
+logging.basicConfig(filename=os.path.join(logs_path,'errors_field.log'), format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
                     level=logging.DEBUG, filemode='a')
 
-def match_stilts(filename, replace=True):
+def match_stilts(filename, replace=True, verbose=True):
+    galex_failed = False
     input_filename = filename
-    file = filename.split(os.path.sep)[-1]
-    # input_filename = os.path.join(raw_path,filename)
-    # input_filename = input_filename.replace(" ", "\ ")
+    file = os.path.basename(input_filename)
 
     galex_filename = os.path.join(save_xmatch_path, file.split('.')[0]+"_temp.fits")
-    # galex_filename = filename.split('.')[0]+"_temp.fits"
-    # galex_filename = galex_filename.replace(" ", "\ ")
-    
+
     final_filename = os.path.join(save_xmatch_path,file)
     
     if os.path.exists(final_filename) and replace==False:
-        #logging.warning("Crossmatch file already exists. Passing.")
+        if verbose:
+            print("Crossmatch file already exists. Passing.")
+        logging.warning("Crossmatch file already exists. Passing.")
         return
-            
-    os.system(f"""java -jar {os.path.join(aux_path,"stilts.jar")} cdsskymatch in={input_filename} cdstable=II/335/galex_ais ra=RA dec=DEC radius=2 find=each blocksize=100000 \\
+    try:
+        os.system(f"""java -jar {os.path.join(aux_path,"stilts.jar")} cdsskymatch in={input_filename} cdstable=II/335/galex_ais ra=RA dec=DEC radius=2 find=each blocksize=100000 \\
                 ocmd='addcol ID_GALEX "objid"; addcol sep_GALEX "angDist"; delcols "objid angDist"' out={galex_filename}""")
+    except:
+        pass
     
+    if os.path.exists(galex_filename) == False:
+        if verbose:
+            print("GALEX crossmatch failed. Passing to unWISE.")
+        logging.warning("GALEX crossmatch failed. Passing to unWISE.")
+        galex_failed = True
 
-    # output_filename = output_filename.replace(" ", "\ ")
-    os.system(f"""java -jar {os.path.join(aux_path,"stilts.jar")} cdsskymatch in={galex_filename} cdstable=II/363/unwise ra=RA dec=DEC radius=2 find=each blocksize=100000 \\
-                ocmd='addcol ID_unWISE "objID"; addcol sep_unWISE "angDist"; delcols "objID angDist"' out={final_filename}""")
-    os.system(f"""rm {galex_filename}""")
+    
+    if galex_failed:
+        if verbose:
+            print("Matching with unWISE.")
+        os.system(f"""java -jar {os.path.join(aux_path,"stilts.jar")} cdsskymatch in={input_filename} cdstable=II/363/unwise ra=RA dec=DEC radius=2 find=each blocksize=100000 \\
+                    ocmd='addcol ID_unWISE "objID"; addcol sep_unWISE "angDist"; delcols "objID angDist"' out={final_filename}""")
 
-    logging.info("Finished crossmatching.") 
+    else:
+        os.system(f"""java -jar {os.path.join(aux_path,"stilts.jar")} cdsskymatch in={galex_filename} cdstable=II/363/unwise ra=RA dec=DEC radius=2 find=each blocksize=100000 \\
+                    ocmd='addcol ID_unWISE "objID"; addcol sep_unWISE "angDist"; delcols "objID angDist"' out={final_filename}""")
+        os.system(f"""rm {galex_filename}""")
+
+    if verbose:
+        print("Finished crossmatching.")
     return
--- code/get_crossmatch.py
+++ code/get_crossmatch.py
@@ -2,14 +2,24 @@ import os
 from tqdm import tqdm
 import glob
 import logging
-from auxiliary.paths import raw_path, logs_path
+from auxiliary.paths import raw_path, logs_path, save_xmatch_path
 from auxiliary.crossmatch import match_stilts
 
 logging.basicConfig(filename=os.path.join(logs_path,'field_error.log'), format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',
                     level=logging.DEBUG, filemode='a')
 
-def get_crossmatch(list_raw, replace=False):
+def get_crossmatch(list_raw, replace=False, diff = True):
     logging.info("get_crossmatch was called for %s fields." % len(list_raw))
+    
+    if diff:
+        replace = False #Only crossmatch fields that have not been crossmatched yet.
+        list_match = glob.glob(os.path.join(save_xmatch_path, "*features.fits"))
+        list_match = [os.path.basename(x) for x in list_match]
+        list_raw = [os.path.basename(x) for x in list_raw]
+        list_diff = list(set(list_raw) - set(list_match))
+        list_raw = [os.path.join(raw_path, x) for x in list_diff]
+        
+    
     for file in tqdm(list_raw):
         try:
             match_stilts(file, replace)
@@ -22,4 +32,4 @@ if __name__ == "__main__":
     logging.info("get_crossmatch.py was called.")
     replace = False
     list_raw = glob.glob(os.path.join(raw_path, "*features.fits"))
-    get_crossmatch(list_raw, replace=replace)
+    get_crossmatch(list_raw, replace=replace, diff = True)
